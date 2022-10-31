f533fd98661bff4d96129e5aa03a98049bc252d3
100ngramjit <sangramjitdutta@gmail.com>
2022-10-31 12:54:08 +0530

    Header added to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,8 @@ import { ThemeProvider } from "@mui/material/styles";
 import { darkTheme, lightTheme } from "../themes/themes";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Outlet } from "react-router-dom";
-import { Toolbar } from "@mui/material";
+import { Toolbar, AppBar } from "@mui/material";
+import Header from "./Header";
 import useLocalStorageState from "Hooks/useLocalStorage";
 
 const Layout = () => {
@@ -15,7 +16,9 @@ const Layout = () => {
   return (
     <ThemeProvider theme={mode === "light" ? lightTheme : darkTheme}>
       <CssBaseline />
-
+      <AppBar enableColorOnDark elevation={16}>
+        <Header changeTheme={changeTheme} mode={mode} />
+      </AppBar>
       <Toolbar />
       <Outlet />
     </ThemeProvider>
