9a8fac7f154e86cb4763c151b9dff8e409543606
100ngramjit <sangramjitdutta@gmail.com>
2022-11-04 18:26:28 +0530

    product component added in listing page and sorting implemented

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -1,18 +1,43 @@
 import {
   Box,
   Container,
+  FormControl,
   Grid,
+  InputLabel,
+  MenuItem,
   Pagination,
+  Select,
+  SelectChangeEvent,
   Skeleton,
   Typography,
 } from "@mui/material";
+import ListingCard from "components/ListingCard";
 import { ChangeEvent, useEffect, useState } from "react";
-import { getProducts } from "services/apiService";
+import { getProducts, getProductsInSortedOrder } from "services/apiService";
 
 const Listing = () => {
   const [page, setPage] = useState(1);
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const sortProducts = async (order: string) => {
+    try {
+      setIsLoading(true);
+      const response = await getProductsInSortedOrder(order, page);
+      setProducts(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e: SelectChangeEvent) => {
+    if (e.target.value === "asc") {
+      sortProducts("asc");
+    } else if (e.target.value === "desc") {
+      sortProducts("desc");
+    }
+  };
 
   useEffect(() => {
     const getProductsList = async () => {
@@ -33,13 +58,34 @@ const Listing = () => {
         <Typography variant="h3" sx={{ m: 2 }}>
           Products
         </Typography>
+        <FormControl
+          variant="standard"
+          sx={{ m: 2, minWidth: 150 }}
+          size="small"
+        >
+          <InputLabel id="sort">Sort By</InputLabel>
+          <Select
+            labelId="sort"
+            id="sortby"
+            defaultValue=""
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"asc"}>price low to high</MenuItem>
+            <MenuItem value={"desc"}>price high to low</MenuItem>
+          </Select>
+        </FormControl>
       </Box>
       <Grid container justifyContent="space-evenly">
         {!isLoading ? (
           products.length === 0 ? (
             <div>no products found</div>
           ) : (
-            products.map((product: any) => <div />)
+            products.map((product: any) => (
+              <ListingCard product={product} key={product.id} />
+            ))
           )
         ) : (
           [...Array(3)].map((e, i) => (
